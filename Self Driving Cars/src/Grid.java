0c691f12b002894217cc712c439fba66badd7dc2
Michael Whitman <mwhitma2@illinois.edu>
2014-10-02 22:03:25 -0500

    Added the basics of a temporary grid class (because map team will probably make their own). Also created a few test maps that load roads, nonroads, and stoplights. These still need to be implemented before anything can be tested.

---
+++ Self Driving Cars/src/Grid.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+ * I am 99% sure that the map generation team will be writing this exact same class only better probably, but I'll write it anyway!
+ * This process could probably be done in a way easier for map creators through picture files
+ * @author mwhitma2
+ *
+ */
+public class Grid {
+	
+	private int width,height;
+	//Tile[][] data;
+	
+	public Grid(int w, int h, String path) {
+		this.width=w;
+		this.height=h;
+		try {
+			load(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void load(String path) throws IOException{
+		Scanner scanner = new Scanner(path);
+		for(int y=0; y<height && scanner.hasNextLine(); y++) {
+			String line = scanner.nextLine();
+			for(int x=0; x<width && x<line.length(); x++) {
+				char t = line.charAt(x);
+				//Process the characters in the text file, put them into the array
+				switch(t) {
+				case '0': //Nonroad
+					break;
+				case '1': //Road
+					break;
+				case '2': //Traffic light
+					break;
+				default:
+					//data[x][y] = null;
+					break;
+				}
+			}
+		}
+		scanner.close();
+	}
+		
+		
+		
+		
+	/*public Tile getTile(int x, int y) {
+		return data[x][y];
+	}*/
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	
+}
