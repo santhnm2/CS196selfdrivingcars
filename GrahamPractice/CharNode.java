cb603be8a0eea5c4b2c02f32bbb417e8c7959500
santhnm2@illinois.edu <santhnm2@linux-a3.ews.illinois.edu>
2014-10-07 22:13:17 -0500

    Fixed optimization folders

---
+++ GrahamPractice/CharNode.java
@@ -0,0 +1,8 @@
+//An object which contains one character and refers to the previous character
+//and the next character.
+public class CharNode {
+   char item;
+   CharNode next;
+   CharNode previous;
+   
+}
