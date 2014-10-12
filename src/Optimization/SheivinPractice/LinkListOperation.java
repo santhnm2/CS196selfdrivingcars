c633374d03c586126b8695ec4c7b0c8d99961a8d
Sheivin Goyal <sheivingoyal@gmail.com>
2014-10-12 03:50:14 -0500

    Edited the program to include the functionality of adding the number as a whole and not just separately as digits.

--- src/Optimization/SheivinPractice/LinkListOperation.java
+++ src/Optimization/SheivinPractice/LinkListOperation.java
@@ -8,19 +8,52 @@ public class LinkListOperation {
 	public static void main(String[] args)throws IOException {
 		LinkList list = new LinkList();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter numbers to be added to Link List");
-		int d = 0;
-		String line = "";
+		
 		while(true) {
-			line = br.readLine();
-			if(line.length() == 0) {
-				break;
-			}
-			d = Integer.parseInt(line);
-			list.insert(d);
+		System.out.println("Menu");
+		System.out.println("1. Enter number as a whole");
+		System.out.println("2. Enter digits separately");
+		System.out.println("3. Exit");
+		System.out.println("Enter your choice");
+		int choice = Integer.parseInt(br.readLine());
+		boolean singleDigit = false;
+		
+		if(choice == 3) {
+			System.out.println("Bye!");
+			System.exit(0);
 		}
+		
+		if(choice == 2) {
+			singleDigit = true;
+		}
+		
+		if(singleDigit) {
+			System.out.println("Enter numbers to be added to Link List");
+			int d = 0;
+			String line = "";
+			while(true) {
+				line = br.readLine();
+				if(line.length() == 0) {
+					break;
+				}
+				d = Integer.parseInt(line);
+				list.insert(d);
+				list.rollingSum();
+			}
+		}
+		else {
+			System.out.println("Enter number");
+			String line = br.readLine();
+			int d = 0;
+			for(int i = 0; i < line.length(); i++) {
+				d = line.charAt(i) - 48;
+				list.insert(d);
+			}
+		}
+		
 		list.printList();
 		list.print();
+		}
 
 	}
 
