22dbf88e1517baa64f1b66fbd4e904595f05022d
Graham Ellis <geellis@Illinois.edu>
2014-11-04 13:08:21 -0600

    cleaned out optimization folder of the practice files.

--- src/Optimization/GrahamPractice/CharNode.java
+++
@@ -1,8 +0,0 @@
-//An object which contains one character and refers to the previous character
-//and the next character.
-public class CharNode {
-   char item;
-   CharNode next;
-   CharNode previous;
-   
-}
