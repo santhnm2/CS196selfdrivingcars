1cac601b188a2e9a52f597a0963e0b4edee0e698
Graham Ellis <geellis@Illinois.edu>
2014-11-04 22:52:53 -0600

    commented instructions for Dijkstra code

--- src/Optimization/Dijkstra.java
+++ src/Optimization/Dijkstra.java
@@ -10,8 +10,14 @@ public class Dijkstra {
 		ArrayList<Integer> path = new ArrayList<Integer>();
 		MapConverter.addNode(graph, car.getXPos(), car.getYPos(), map);
 		MapConverter.addNode(graph, car.getDestX(), car.getDestY(), map);
+		//put start position in priority queue with 'weight' of 0
+		PriorityQueue shortest = new PriorityQueue();
+		shortest.add(0, MapConverter.nodeIndex(graph, car.getXPos(), car.getYPos()), -1);
+			//heap the queue
+			//pop the top 
+			//for each of the top's children, weight becomes weight of top + their weight and parent index is top
 		
-		
+		//repeat until you pop the destination
 		
 		return path;
 		
