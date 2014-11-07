62ad42cbb0aff5622a7f929ab99003d49ee4800d
Graham Ellis <geellis@Illinois.edu>
2014-11-06 21:39:16 -0600

    added some comments, nodes now require road between them.

--- src/Optimization/Dijkstra.java
+++ src/Optimization/Dijkstra.java
@@ -4,111 +4,119 @@ import Car.Car;
 import Map.Map;
 import Constants.Directions;
 import java.util.ArrayList;
+
+/*PROBLEMS:
+ *the toArrayList function doesn't create a path which stays in the right lane.
+ *this is related to the fact that all there are 4 nodes per intersection.
+ *also the fact that edges aren't directional.
+ */
 public class Dijkstra {
-   //take in a converted map, take in the coordinates of a starting point and a destination, and add those two nodes to the converted map.
-   //find the shortest path between those two points. Return an arraylist with the directions.
-   public static ArrayList<Integer> findPath(Map map, graphNode[] graph, Car car){
+   // take in a converted map, take in the coordinates of a starting point and a
+   // destination, and add those two nodes to the converted map.
+   // find the shortest path between those two points. Return an arraylist with
+   // the directions.
+   public static ArrayList<Integer> findPath(Map map, graphNode[] graph, Car car) {
       ArrayList<Integer> path = new ArrayList<Integer>();
       int[][] completed = new int[2][0];
       MapConverter.addNode(graph, car.getXPos(), car.getYPos(), map);
       MapConverter.addNode(graph, car.getDestX(), car.getDestY(), map);
-      //put start position in priority queue with 'weight' of 0
+      // put start position in priority queue with 'weight' of 0
       PriorityQueue pq = new PriorityQueue();
       pq.add(0, MapConverter.nodeIndex(graph, car.getXPos(), car.getYPos()), -1);
       boolean destination = false;
-      while(!destination) {
-         //heap the queue
+      while (!destination) {
+         // heap the queue
          pq.heapMyArray();
-         //pop the top 
+         // pop the top
          int[] current = pq.remove();
-         if (finalDestOneDirection(graph[current[1]].getX(), car.getDestX()) && finalDestOneDirection(graph[current[1]].getY(), car.getDestY()))
+         if (finalDestOneDirection(graph[current[1]].getX(), car.getDestX())
+               && finalDestOneDirection(graph[current[1]].getY(), car.getDestY()))
             destination = true;
          else
             relax(current, graph, pq);
          completedItem(completed, current);
-      }     
-      
-      //repeat until you pop the destination
+      }
+
+      // repeat until you pop the destination
       toArrayList(path, completed, completed[0].length, graph);
       return path;
    }
 
-   public static void toArrayList(ArrayList<Integer> path, int[][] arr, int index, graphNode[] graph){
-      //converts an array of connected items to an an ArrayList.      
+   public static void toArrayList(ArrayList<Integer> path, int[][] arr, int index, graphNode[] graph) {
+      // converts an array of connected items to an an ArrayList.
+      // as stated at the top, it doesn't stay in the right lane to get to the
+      // next node.
       if (arr[1][index] != -1)
          toArrayList(path, arr, arr[1][index], graph);
       else
          return;
-     moveInDirection(graph[arr[1][index]], graph[arr[0][index]], path);
-     
-         
+      moveInDirection(graph[arr[1][index]], graph[arr[0][index]], path);
+
+   }
+
+   public static void relax(int[] current, graphNode[] graph, PriorityQueue pq) {
+      int pos = current[1];
+      int weight = current[0];
+
+      // Adding all the new weights for all four directions to the priority
+      // queue
+      if (graph[pos].getUp() != null) {
+         weight += graph[pos].getY() - graph[pos].getUp().getY();
+         pq.add(weight, graph[pos].getUp().getIndex(), pos);
       }
-   
-	public static void relax(int[] current, graphNode[] graph, PriorityQueue pq) {
-		int pos = current[1];
-		int weight = current[0];
-		
-		//Adding all the new weights for all four directions to the priority queue
-		if(graph[pos].getUp() != null) {
-			weight += graph[pos].getY() - graph[pos].getUp().getY();
-			pq.add(weight, graph[pos].getUp().getIndex(), pos);
-		}
-		if(graph[pos].getRight() != null) {
-			weight += graph[pos].getRight().getX() - graph[pos].getX();
-			pq.add(weight, graph[pos].getRight().getIndex(), pos);
-		}
-		if(graph[pos].getDown() != null) {
-			weight += graph[pos].getDown().getY() - graph[pos].getY();
-			pq.add(weight, graph[pos].getDown().getIndex(), pos);
-		}
-		if(graph[pos].getLeft() != null) {
-			weight += graph[pos].getX() - graph[pos].getLeft().getX();
-			pq.add(weight, graph[pos].getLeft().getIndex(), pos);
-		}
-	}
-   public static void completedItem(int[][] completed, int[] current){
-      int[][] temp = new int[0][completed[0].length+1];
-      for (int i = 0; i< completed[0].length; i++){
+      if (graph[pos].getRight() != null) {
+         weight += graph[pos].getRight().getX() - graph[pos].getX();
+         pq.add(weight, graph[pos].getRight().getIndex(), pos);
+      }
+      if (graph[pos].getDown() != null) {
+         weight += graph[pos].getDown().getY() - graph[pos].getY();
+         pq.add(weight, graph[pos].getDown().getIndex(), pos);
+      }
+      if (graph[pos].getLeft() != null) {
+         weight += graph[pos].getX() - graph[pos].getLeft().getX();
+         pq.add(weight, graph[pos].getLeft().getIndex(), pos);
+      }
+   }
+
+   public static void completedItem(int[][] completed, int[] current) {
+      int[][] temp = new int[0][completed[0].length + 1];
+      for (int i = 0; i < completed[0].length; i++) {
          temp[0][i] = completed[0][i];
          temp[1][i] = completed[1][i];
       }
       temp[0][completed[0].length] = current[1];
-      temp[1][completed[0].length] = current[2];      
+      temp[1][completed[0].length] = current[2];
    }
+
    public static Boolean finalDestOneDirection(int start, int end) {
       if (start == end || start + 1 == end || start - 1 == end)
          return true;
       return false;
    }
-   
-   //function to add one direction to the list at a time
+
+   // function to add one direction to the list at a time
    public static void moveInDirection(graphNode a, graphNode b, ArrayList<Integer> path) {
-	   if(a.getX() == b.getX()) {
-		   if(a.getY() > b.getY()) {
-			   for(int i = 0; i < a.getY() - b.getY(); i++){
-				   path.add(Directions.UP);
-			   }
-		   }
-		   else {
-			   for(int i = 0; i < b.getY() - a.getY(); i++) {
-				   path.add(Directions.DOWN);
-			   }
-		   }
-	   }
-	   else {
-		   if(a.getX() > b.getX()) {
-			   for(int i = 0; i < a.getX() - b.getX(); i++) {
-				   path.add(Directions.LEFT);
-			   }
-		   }
-		   else {
-			   for(int i = 0; i < b.getX() - a.getX(); i++) {
-				   path.add(Directions.RIGHT);
-			   }
-		   }
-	   }
+      if (a.getX() == b.getX()) {
+         if (a.getY() > b.getY()) {
+            for (int i = 0; i < a.getY() - b.getY(); i++) {
+               path.add(Directions.UP);
+            }
+         } else {
+            for (int i = 0; i < b.getY() - a.getY(); i++) {
+               path.add(Directions.DOWN);
+            }
+         }
+      } else {
+         if (a.getX() > b.getX()) {
+            for (int i = 0; i < a.getX() - b.getX(); i++) {
+               path.add(Directions.LEFT);
+            }
+         } else {
+            for (int i = 0; i < b.getX() - a.getX(); i++) {
+               path.add(Directions.RIGHT);
+            }
+         }
+      }
    }
 
 }
-	
-
--- src/Optimization/MapConverter.java
+++ src/Optimization/MapConverter.java
@@ -2,9 +2,14 @@ package Optimization;
 
 import Map.*;
 import Map.Road.*;
-
+import Map.NonRoad.*;
+/*PROBLEMS:
+*currently the MapConverter stores each intersection as four nodes. It would be better if it was one node.
+*destination is stored as a node on only one side of the road. the other side isn't recognized as the destination.
+*
+*/
 public class MapConverter {
-   // converts a given map to a graph w/ vertexes at traffic lights.
+   // converts a given map to a graph w/ vertices at traffic lights.
    public static graphNode[] toGraph(Map map) {
       graphNode[] graph = new graphNode[0];
       for (int x = 0; x < map.getLengthX(); x++)
@@ -26,6 +31,9 @@ public class MapConverter {
     	  if(nodeIndex(graph, x, i) != -1) {
     		  temp[graph.length].setUp(temp[nodeIndex(graph, x, i)]);
     		  temp[nodeIndex(graph, x, i)].setDown(temp[graph.length]);
+           //forces the for loop to end because there is a break in the road.
+           if (map.get(x, i) instanceof NonRoad)
+              i = 0;
     	  }
       }
       // search left until you get to a node and then set this node's left and that
@@ -34,24 +42,31 @@ public class MapConverter {
     	  if(nodeIndex(graph, i, y) != -1) {
     		  temp[graph.length].setLeft(temp[nodeIndex(graph, i, y)]);
     		  temp[nodeIndex(graph, i, y)].setRight(temp[graph.length]);
+           //forces the for loop to end because there is a break in the road.
+           if (map.get(i, x) instanceof NonRoad)
+              i = 0;
     	  }
       }
       // search down until you get to a node and then set this node's down and that
-      // node's up. This is done because it will help when we add the source and the
-      // destination as nodes.
+      // node's up. This is done only when we add a source or a destination.
       for(int i = y; i < map.getLengthY(); i++) {
     	  if(nodeIndex(graph, x, i) != -1) {
     		  temp[graph.length].setDown(temp[nodeIndex(graph, x, i)]);
     		  temp[nodeIndex(graph, x, i)].setUp(temp[graph.length]);
+           //forces the for loop to end because there is a break in the road.
+           if (map.get(x, i) instanceof NonRoad)
+              i = map.getLengthY();
     	  }
       }
       // search right until you get to a node and then set this node's right and that
-      // node's left. This is done because it will help when we add the source and the
-      // destination as nodes.
+      // node's left.  This is done only when we add a source or a destination.
       for(int i = x; i < map.getLengthX(); i++) {
     	  if(nodeIndex(graph, i, y) != -1) {
     		  temp[graph.length].setRight(temp[nodeIndex(graph, i, y)]);
     		  temp[nodeIndex(graph, i, y)].setLeft(temp[graph.length]);
+    		//forces the for loop to end because there is a break in the road.
+           if (map.get(i, y) instanceof NonRoad)
+              i = map.getLengthX();
     	  }
       }
    }
