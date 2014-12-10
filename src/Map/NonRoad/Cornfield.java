a9b71f6538d4b8a30ccf6a707ae2761a27b95fc6
Arshdeep Sabharwal <asabhar2@illinois.edu>
2014-12-09 22:49:25 -0600

    Fixed density crap

---
+++ src/Map/NonRoad/Cornfield.java
@@ -0,0 +1,12 @@
+package Map.NonRoad;
+
+public class Cornfield extends NonRoad {
+
+    public Cornfield(int x, int y) {
+        super(x, y, .0);
+    }
+
+    public String toString(){
+        return "..";
+    }
+}
---
+++ src/Map/NonRoad/Mall.java
@@ -0,0 +1,12 @@
+package Map.NonRoad;
+
+public class Mall extends NonRoad {
+
+    public Mall(int x, int y) {
+        super(x, y, 4);
+    }
+
+    public String toString(){
+        return "..";
+    }
+}
